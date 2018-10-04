543553b684f8f00bf2eb7fca1aa52cb7bf1be865
kevin <kevin@kevin>
2018-10-04 13:00:46 +0200

    Request Cambio en el Control mas parametros de request

--- src/main/java/net/daw/control/json.java
+++ src/main/java/net/daw/control/json.java
@@ -7,6 +7,7 @@ package net.daw.control;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -38,22 +39,37 @@ public class json extends HttpServlet {
             out.println("<title>Servlet json</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet json at " + request.getContextPath() + "</h1>");
-            out.println("<h4>" + request.authenticate(response) + "</h4>");
-            out.println("<h4>" + request.getCookies()+ "</h4>");
-            out.println("<h4>" + request.getLocalPort() + "</h4>");
-            out.println("<h4>" + request.getServerPort() + "</h4>");
-            out.println("<h4>" + request.getSession() + "</h4>");
-            out.println("<h4>" + request.getPathInfo() + "</h4>");
-            out.println("<h4>" + request.getRequestURI() + "</h4>");
-            out.println("<h4>" + request.getUserPrincipal() + "</h4>");
+            out.println("<h2>Servlet json esta en " + request.getContextPath() + "</h2>");            
+            out.println("<h4>" + Arrays.toString(request.getCookies())+ "</h4>");                                          
             out.println("<h4>" + request.getMethod() + "</h4>");
-            out.println("<h4>" + request.getLocalName() + "</h4>");
+            out.println("<h4>" + request.getLocalName() + "</h4>");            
+            out.println("<h4>" + request.getLocalPort() + "</h4>"); 
+            out.println("<h4>" + request.getLocale() + "</h4>"); 
+            out.println("<h4>" + request.getLocalAddr()+ "</h4>");  
+            out.println("<h4>" + request.getScheme() + "</h4>");   
             out.println("<h4>" + request.getServerName() + "</h4>");
-            out.println("<h4>" + request.getServletPath() + "</h4>");
+            out.println("<h4>" + request.getServletPath() + "</h4>");            
+            out.println("<h4>" + request.getServletContext() + "</h4>");
+            out.println("<h4>" + request.getServerPort() + "</h4>");
+            out.println("<h4>" + request.getSession() + "</h4>");      
             out.println("<h4>" + request.getProtocol() + "</h4>");
-            out.println("<h4>" + request.getScheme() + "</h4>");            
-            out.println("<h4>" + request.getAuthType() + "</h4>"); 
+            out.println("<h4>" + request.getRemotePort() + "</h4>");
+            out.println("<h4>" + request.getRequestURI() + "</h4>");   
+            out.println("<h4>" + request.getRemoteHost()+ "</h4>");  
+            out.println("<h4>" + request.getRequestedSessionId() + "</h4>");  
+            out.println("<h4>" + request.getReader() + "</h4>"); 
+            out.println("<h4>" + request.getClass() + "</h4>");             
+            out.println("<h4>" + request.getContentLength()+ "</h4>");  
+            out.println("<h4>" + request.getContextPath()+ "</h4>");                          
+            
+            
+            out.println("<h4>" + request.getRemoteUser()+ "</h4>");            
+            out.println("<h4>" + request.getPathTranslated()+ "</h4>");            
+            out.println("<h4>" + request.getAuthType() + "</h4>");
+            out.println("<h4>" + request.getQueryString() + "</h4>");            
+            out.println("<h4>" + request.getPathInfo() + "</h4>");
+            out.println("<h4>" + request.authenticate(response) + "</h4>");
+            out.println("<h4>" + request.getUserPrincipal() + "</h4>");
             out.println("<h4>" + request + "</h4>");           
             out.println("</body>");
             out.println("</html>");
@@ -100,4 +116,6 @@ public class json extends HttpServlet {
     }// </editor-fold>
 
 }
+
+
 
